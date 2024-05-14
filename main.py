d0312e253b02c4ebe88b6d71bbb275e6b0806c7f
Pandaptable <panda@pandaptable.moe>
2024-05-14 17:16:12 -0400

    Closes #1. Fix: delay updated to 0.2 (0.15 wasn't 100% consistent)

--- main.py
+++ main.py
@@ -11,14 +11,23 @@ CONSOLE_FILE = os.getenv('CONSOLE_FILE')
 EXEC_FILE = os.getenv('EXEC_FILE')
 
 def listen(logFile):
-    logFile.seek(0,2)
+    logFile.seek(0, os.SEEK_END)
+    last_size = logFile.tell()
+
     while True:
+        current_size = os.stat(logFile.name).st_size
+        if current_size < last_size:
+            logFile.seek(0, os.SEEK_SET)
+            last_size = current_size
+
         line = logFile.readline()
         if not line:
             time.sleep(0.1)
             continue
-        print(line)
+
+        print(line.strip())
         parse(line)
+        last_size = logFile.tell()
 
 def parse(line):
     regex = re.search(r"\[(?:ALL|(?:C)?(?:T)?)\]\s+(.*)‎(?:﹫\w+)?\s*(?:\[DEAD\])?:(?:\s)?(\S+)?\s(\S+)?", line, flags=re.UNICODE)
@@ -71,12 +80,17 @@ def write_command(command):
         f.write(command)
 
 def press_key():
-    time.sleep(0.15)
+    time.sleep(0.2)
     pyautogui.press('multiply')
 
 def press_key_no_delay():
     pyautogui.press('multiply')
 
+
 if __name__ == '__main__':
-    logFile = open(CONSOLE_FILE,"r", encoding="utf-8")
-    logLines = listen(logFile)
+    log_file = open(CONSOLE_FILE,"r", encoding="utf-8")
+    try:
+        while True:
+            listen(log_file)
+    except KeyboardInterrupt:
+        print("galls gone")
